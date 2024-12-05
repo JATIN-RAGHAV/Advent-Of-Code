5bd6ef97262f80c6ff1382384a1a36fe90051464
JATIN-RAGHAV <jrag137@gmail.com>
2024-12-05 17:28:05 +0530

    Completed Day 5

---
+++ Day-05/part1.js
@@ -0,0 +1,48 @@
+import fs from 'fs';
+
+let updates = fs.readFileSync('text.txt', 'utf8').split('\n');
+let rules = updates.splice(0, updates.indexOf('') + 1)
+
+rules.pop();
+updates.pop();
+
+rules = rules.map(row => {
+	return row.split('|').map(num => parseInt(num))
+})
+
+updates = updates.map(row => {
+	return row.split(',').map(num => parseInt(num))
+})
+
+let rulesObject = {};
+
+for (let i = 0; i < rules.length; i++) {
+	if (rulesObject[rules[i][1]])
+		rulesObject[rules[i][1]].push(rules[i][0]);
+	else
+		rulesObject[rules[i][1]] = [rules[i][0]];
+}
+
+const isCorrectOrder = (update) => {
+	for (let i = 0; i < update.length - 1; i++) {
+		for (let j = i + 1; j < update.length; j++) {
+			if (rulesObject[update[j]] == undefined) {
+				return false;
+			}
+			if (rulesObject[update[j]].indexOf(update[i]) == -1) {
+				return false;
+			}
+		}
+	}
+	return true;
+}
+
+let sumOfMiddle = 0;
+
+for (let i = 0; i < updates.length; i++) {
+	const row = updates[i]
+	if (isCorrectOrder(row))
+		sumOfMiddle += row[(row.length - 1) / 2]
+}
+
+console.log(sumOfMiddle);
